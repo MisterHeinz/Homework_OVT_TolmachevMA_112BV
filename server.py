063de4ea5b09b6173ed9cef39ca1cb9efe950621
Maxim Tolmachev <maximtolmachev@Maxims-MacBook-Air.local>
2026-05-28 11:11:12 +0300

    Add simple database and README

--- server.py
+++ server.py
@@ -1,26 +1,37 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
 
-status = 'OFF'
+if not os.path.exists("status.txt"):
+    with open("status.txt", "w") as f:
+        f.write("OFF")
+
+def updateStatus(new_status):
+    with open("status.txt", "w") as data_base:
+        data_base.write(new_status)
+
+def readStatus():
+    with open("status.txt", "r") as data_base:
+        return data_base.read()
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        global status
         
         if self.path == '/status-on':
-            status = 'ON'
+            updateStatus('ON')
             self.send_response(200)
             self.end_headers()
-            self.wfile.write(b'OK')
+            self.wfile.write(b'Status changed')
         
         elif self.path == '/status-off':
-            status = 'OFF'
+            updateStatus('OFF')
             self.send_response(200)
             self.end_headers()
-            self.wfile.write(b'OK')
+            self.wfile.write(b'Status changed')
         
         else:
             self.send_response(200)
             self.end_headers()
+            status = readStatus()
             self.wfile.write(f'STATUS: {status}'.encode())
 
 print('Сервер запущен: http://localhost:3000')
